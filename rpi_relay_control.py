80e32423c89f2c0db3dab47fbbd43c2a1b336f79
lifeonhoth <fhbach@gmail.com>
2017-01-06 16:17:10 -0500

    first version complete - ready for testing

--- rpi_relay_control.py
+++ rpi_relay_control.py
@@ -1,11 +1,40 @@
 import RPi.GPIO as GPIO
 import time
 
+# use BCM or BOARD, but note that they have different pin layouts
 GPIO.setmode(GPIO.BOARD)
 
 # this is our pin on the rpi that connects to the relay
 relayPin = 3
 
+# this is how long the relay stays on
+sleepDuration = 5
+
+# setting up gpio pin
 GPIO.setup(relayPin, GPIO.OUT)
 GPIO.output(relayPin, GPIO.HIGH)
+
+try:
+	# turn on the relay
+	GPIO.output(relayPin, GPIO.LOW)
+	print "Turning Relay ON"
+	# sleep
+	time.sleep(sleepDuration)
+	# turn off the relay
+	GPIO.output(relayPin, GPIO.HIGH)
+	print "Turning Relay OFF"
+
+	# reset gpio pin
+	GPIO.cleanup()
+
+except KeyboardInterrupt:
+	print " Quit"
+
+	# reset gpio pin
+	GPIO.cleanup()
+
+
+
+
+
 
